64d11c0e6a0b5672be20623a8c7f054e51c24045
stenlitong <stenli.tong@binus.ac.id>
2022-03-21 16:39:55 +0800

    testing update doclegal_V2

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,8 @@ class User extends Authenticatable implements MustVerifyEmail
     use LaratrustUserTrait;
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
      *
