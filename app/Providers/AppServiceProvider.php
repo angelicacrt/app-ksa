56f7baf64d0c9a1ce2021d6e1d43c70bd597dc0e
stenlitong <stenli.tong@binus.ac.id>
2022-03-21 16:27:38 +0800

    testing update doclegal_V1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {   
         // on .env change to https://localhost
-        URL::forceScheme('http');
+        URL::forceScheme('https');
         Paginator::useBootstrap();
     }
 }
